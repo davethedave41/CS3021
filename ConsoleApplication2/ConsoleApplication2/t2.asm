b3a1f86c19aef48e558184aa05880f9421e74312
David Urdaibay <urdaibad@tcd.ie>
2019-09-28 19:01:35 +0100

    Fixed pushing parameters to functions and confused on how to implement function q lmao

--- ConsoleApplication2/ConsoleApplication2/t2.asm
+++ ConsoleApplication2/ConsoleApplication2/t2.asm
@@ -1,11 +1,15 @@
 option casemap:none             ; case sensitive
 
-;includelib legacy_stdio_definitions.lib
-;extrn printf:near
+includelib legacy_stdio_definitions.lib
+extrn printf:near
 
-.data ; start of a data section
+.data						    ; start of a data section
+
+str	db 'a = %I64d b = %I64d c = %I64d d = %I64d e = %I64d sum = %I64d\n', 0AH, 00H		;ASCII format string
+
 public g						; export variable g
 g	QWORD	4					; declare global variable g initialised to 4
+
 .code							; start of a code section
 
 public min						; export function name
@@ -22,19 +26,21 @@ min2:    ret					; return rax
 public p						; export function name
 
 p:       sub	rsp, 32         ; allocate 32 bytes of shadow space
-		 push	rdx			    ; push j
-		 push	rcx				; push i
-		 push	g					; push g
+		 mov	[rsp + 40], r8  ; save k
+		 mov	r8, rdx	        ; push j
+		 mov	rdx, rcx		; push i
+		 mov 	rcx, g			; push g
 		 call	min				; min(g, i, j)
-		 push	r9				; push l
-		 push	r8				; push k
-		 push	rax				; push min(g, i, j)
+		 mov	r8, r9			; push l
+		 mov 	rdx, [rsp + 40] ; push k
+		 mov 	rcx, rax		; push min(g, i, j)
 		 add	rsp, 32			; deallocate shadow space
 		 ret					; return rax
 
 public gcd						; export function name
 
 gcd:	 sub	rsp, 32		    ; allocate 32 bytes of shadow space
+		 mov    [rsp + 40], rdx ; save b 
 		 cmp	rdx, 0			; if( b == 0)
 		 jne	gcd1			; -> gcd1 if b != 0
  		 mov	rax, rcx		; rax = a
@@ -43,16 +49,24 @@ gcd1:    mov	rax, rcx		; making rax the dividend
 		 mov	r9, rdx			; can't use rdx here
 		 cdq					; clears rdx
 	     idiv   r9				; a div b
-		 push	rdx				; push a % b
-		 push   r9			    ; push b
+		 mov  	rcx, [rsp + 40  ; push b (remainder stored in rdx and rdx is 2nd parameter slot)
+		 call   gcd				; gcd(b, a % b)
 gcd2:    add	rsp, 32			; deallocate 32 bytes of shadow space
 	     ret					; return rax
 
 public q						: export function name
 		
-		 push rbp
-
 q:		 sub rsp, 32			; allocate shadow space
-		 cmp rdx
+		 mov rax, rcx			; sum = a
+		 add rax, [rsp + 32]	; sum + e
+		 add rax, r9			; sum + d
+		 add rax, r8			; sum + c
+		 add rax, rdx			; sum + b
+		 mov [rsp+40], rax		; param 7 = sum
+		 mov rcx, [rsp]
+		 
+
+		 add rsp, 32			; deallocate shadow space
+		 ret					; return rax
 
 end
