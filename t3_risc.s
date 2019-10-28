8b66207e5a27d67f9f6f40d6878849c1dc9a09d5
David Urdaibay <urdaibad@tcd.ie>
2019-10-28 12:24:12 +0000

    Now ready to hit the boof

--- t3_risc.s
+++ t3_risc.s
@@ -1,6 +1,6 @@
         ; global variable g in r9
         add r0, #4, r9
-        ; int mint(int a, int b, int c)
+        ; int mint(int a, int b, int c) -optimised
         add r0, r26, r1         ; a = p1
         sub r27, r1, r0 {C}     ; if(b > v)
         jge min0                    
@@ -13,35 +13,30 @@ min0:   sub r28, r1, r0 {C}     ; if(c > v)
 min1:   ret r25, 0              ; return in r25
         xor r0, r0, r0          ; not possible to remove any of the nops in the delay slots
 
-        ; int p(int i, int j, int k, int l)
+        ; int p(int i, int j, int k, int l) -optimised
         add r9, r0, r10         ; pass g
         add r26, r0, r11        ; pass i
-        add r27, r0, r12        ; pass j
         callr r25, min          ; min(g,i,j)
-        xor r0, r0, r0          ; nop
+        add r27, r0, r12        ; pass j
         add r1, r0, r10         ; pass min(g,i,j)
         add r28, r0, r11        ; pass k
-        add r29, r0, r12        ; pass l
         callr r25, min          ; min(min(g,i,j),k,l)
-        xor r0, r0, r0          ; nop
+        add r29, r0, r12        ; pass l
         ret r25, 0              ; return
         xor r0, r0, r0          ; nop
 
-        ;int gcd(int a, int b)
+        ;int gcd(int a, int b) -optimised
         sub r27, r0, r0, {C}    ; if(b == 0)
         jne gcd0                ; ->gcd0
         xor r0, r0, r0          ; nop
-        add r26, r0, r1         ; return a
         jmp gcd1                ; go to return
-        xor r0, r0, r0          ; nop
+        add r26, r0, r1         ; return a
 gcd0:   add r26, r0, r10        ; passing a into mod(a,b)
-        add r27, r0, r11        ; passing b into mod(a,b)
         callr r25, mod          ; external function mod called
-        xor r0, r0, r0,         ; nop
-        add r27, r0, r10        ; passing b
-        add r1, r0, r11         ; passing mod(a,b)     
+        add r27, r0, r11        ; passing b into mod(a,b)
+        add r27, r0, r10        ; passing b    
         callr r25, gcd          ; gcd(b, a % b)
-        xor r0, r0, r0          ; nop
+        add r1, r0, r11         ; passing mod(a,b) 
 gcd1:   ret r25, 0              ; return 
         xor r0, r0, r0          ; nop
 
